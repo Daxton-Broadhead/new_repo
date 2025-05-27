3b4bbcc4aa30bd7242088eb5ef0be7b1307483d7
Daxton Broadhead <daxton.broadhead21@gmail.com>
2025-05-27 18:53:50 -0400

    I don't know if this is going to work

---
+++ build/cpp_pubsub/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/daxton/ros2_beginning/src/cpp_pubsub
+# Build directory: /home/daxton/ros2_beginning/build/cpp_pubsub
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/turtlesim/colcon_command_prefix_test.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/daxton/ros2_beginning/install/turtlesim/share/turtlesim/package.sh"
